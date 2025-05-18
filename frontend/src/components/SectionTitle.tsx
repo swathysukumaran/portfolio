df7e223efa3f2b2ffb0f30d059f8d0e483bd6963
swathysukumaran <swathysukumaran@gmail.com>
2025-05-18 12:35:23 -0700

    Update about me style

--- frontend/src/components/SectionTitle.tsx
+++ frontend/src/components/SectionTitle.tsx
@@ -6,7 +6,7 @@ type SectionTitleProps = {
 function SectionTitle({ title }: SectionTitleProps) {
   return (
     <div className="flex gap-10 items-center py-10">
-      <h1 className="text-3xl text-white ">{title}</h1>
+      <h1 className="text-3xl">{title}</h1>
       <div className="w-50 h-[1px] bg-tertiary"></div>
     </div>
   );
--- frontend/src/pages/Home/About.tsx
+++ frontend/src/pages/Home/About.tsx
@@ -16,40 +16,40 @@ function About() {
 
   return (
     <section
-      className="w-full px-6 md:px-24 py-20 bg-primary text-secondary"
+      className="w-full px-6 md:px-24 py-24 bg-primary text-secondary"
       id="about"
     >
       <SectionTitle title="About Me" />
 
-      <div className="mt-10 max-w-4xl flex flex-col gap-6">
-        <p className="text-lg leading-relaxed">
+      <div className="mt-12 max-w-4xl  flex flex-col gap-8 text-base sm:text-lg leading-relaxed">
+        <p className="text-secondary">
           I’m someone who finds joy in making things work — especially when they
           don’t at first. I enjoy the quiet thrill of solving bugs, learning new
           tools, and that moment when hours of stuckness suddenly turn into
-          “it’s working!!!”
+          <span className="text-tertiary font-medium"> “it’s working!!!”</span>
         </p>
 
-        <p className="text-lg leading-relaxed">
+        <p className="text-secondary">
           For me, good software isn’t just about code — it’s about real people
           being able to use it comfortably. I care about building things that
           feel smooth, clear, and helpful.
         </p>
 
-        <p className="text-lg leading-relaxed">
+        <p className="text-secondary">
           Outside of coding, I’m a mom and a lifelong reader. I value calm,
           kindness, and staying curious — and I try to bring that into the way I
           work too.
         </p>
 
-        <div>
-          <h2 className="text-tertiary text-xl font-medium mb-3">
+        <div className="mt-6">
+          <h2 className="text-tertiary text-lg sm:text-xl font-medium mb-4">
             Tools and technologies I use:
           </h2>
           <div className="flex flex-wrap gap-3">
             {skills.map((skill, index) => (
               <span
                 key={index}
-                className="text-tertiary border border-tertiary px-4 py-2 rounded text-sm"
+                className="text-tertiary border border-tertiary px-4 py-2 rounded-full text-sm hover:bg-tertiary hover:text-primary transition"
               >
                 {skill}
               </span>
