ba1b3af08048efa9827c84e5ce4b2a583028a8a5
swathysukumaran <swathysukumaran@gmail.com>
2025-05-19 00:22:27 -0700

    Ensure scroll to sections

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,8 +4,21 @@ import About from "./About";
 import Experiences from "./Experiences";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function Home() {
+  const location = useLocation();
+
+  // Scroll to the element with the ID from the URL hash when the component mounts
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.querySelector(location.hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
   return (
     <div>
       <Header />
